f904d7af8470c36fe41ebbf17f25f3ba29bea60f
gilmujjang <alsanrlf@naver.com>
2022-06-11 18:19:53 +0900

    feat: png 이미지 나오게함

--- src/Assets/cssData.ts
+++ src/Assets/cssData.ts
@@ -1,3 +1,5 @@
+import moving_taxi from "./img/moving_taxi.png";
+
 interface Items {
   source: string;
   title: string;
@@ -7,17 +9,17 @@ interface Items {
 
 const cssData: Array<Items> = [
   {
-    source: 'https://www.youtube.com/watch?v=WJqd8Sof7lE',
-    title: 'Moving Taxi',
-    link: 'MovingTaxi',
-    image: ''
+    source: "https://www.youtube.com/watch?v=WJqd8Sof7lE",
+    title: "Moving Taxi",
+    link: "MovingTaxi",
+    image: moving_taxi,
   },
   {
-    source: 'https://www.youtube.com/watch?v=MmdKeypSxE8&t=128s',
-    title: '3D Menu Hover Effects',
-    link: 'D3Menu',
-    image: ''
-  }
+    source: "https://www.youtube.com/watch?v=MmdKeypSxE8&t=128s",
+    title: "3D Menu Hover Effects",
+    link: "D3Menu",
+    image: "",
+  },
 ];
 
 export default cssData;
--- src/components/LinkBox/index.tsx
+++ src/components/LinkBox/index.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import cssData from '../../Assets/cssData';
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import cssData from "../../Assets/cssData";
 
 const LinkBox = () => (
   <>
     {cssData.map((item) => (
       <Link to={item.link} key={`key_of_${item.link}`}>
         {item.title}
+        <Image src={item.image} alt="????" />
       </Link>
     ))}
   </>
 );
+
+const Image = styled.img`
+  width: 256px;
+  height 160px;
+`;
 
 export default LinkBox;
